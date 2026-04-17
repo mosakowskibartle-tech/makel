4c0f721c7e8b412878996b7c90703c314b7122fc
mosakowskibartle-tech <mosakowskibartle@gmail.com>
2026-04-17 13:34:31 +0300

    Update images.ts

--- src/config/images.ts
+++ src/config/images.ts
@@ -9,7 +9,7 @@ export const images = {
   videos: { heroBg: 'https://raw.githubusercontent.com/mosakowskibartle-tech/makel/main/public/videos/hero-bg.mp4' },
   stickyProducts: { first: '/images/rozparal.png', second: '/images/rozparal.png' },
   interiors: { livingRoom: '/images/loft.jpg', bathroom: '/images/vanna.jpg', kitchen: '/images/kuhna.jpg', bedroom: '/images/spal.jpg' },
-  about: { factory: '/images/zavod1.jpg', background: '/images/mnogoroz.jpeg', global: '/images/rozet3logo.png', techFactory: '/images/zavod.jpg' },
+  about: { factory: '/images/zavod1.png', background: '/images/mnogoroz.jpeg', global: '/images/rozet3logo.png', techFactory: '/images/zavod.png' },
   catalog: { lillium: '/images/lilium.jpg', lilliumNatural: '/images/lilium natural.jpg', lilliumKare: '/images/lilium kare.jpg', mimoza: '/images/mimoza.jpg', defne: '/images/defne.jpg', manolya: '/images/manolya.jpg', karea: '/images/karea.jpg', karealine: '/images/karea line.jpg', karearizz: '/images/karea rizz.jpg' },
   outdoor: { ip20: '/images/out.jpg', ip44: '/images/ip4454.jpg', ip55: '/images/ip55.jpg' },
   modular: { lilliumram: '/images/defneram.jpg', mimozaram: '/images/momozaram.jpg', defneram: '/images/defneram.jpg', manolyaram: '/images/manolyaram.jpg', karearam: '/images/karearam.jpg', karealineram: '/images/karealineram.jpg', karearizzram: '/images/karearizram.jpg' },
